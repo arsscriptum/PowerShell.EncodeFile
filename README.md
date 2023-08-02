0f3ed700229d4befc9306f1fc20d1b0b5bca4002
arsscriptum <guillaumeplante.qc@gmail.com>
2023-08-02 09:34:11 -0400

    auto

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
-# Script Encoder
+# PowerShell Proof-of-Concept Converter / File Encoder
 
-Powershell script to encode scripts in another file
+These 2 scripts are used to encode files in other files.
+
+
+## Converters - ConvertFrom-HeaderBlock / ConvertTo-HeaderBlock
+
+These 2 functions are used to include a file (scripts, binary, etc) as a resource in another text file. The latter is represented as a text header like this:
+
+```
+  <# === BEGIN EMBEDDED FILE HEADER === 
+  H 4 s I A A A A A A A A C p 1 W X U / b M B R 9 r 9 T / c F V V a q K R a H t
+  F q g S U d m J j r K I V L 6 x C J r l t g l y 7 2 A 5 d B f z 3 X T s f N Q
+  y I h 0 C D 6 j M V E Y W q Q S t R a m x a T t / A F v B l 2 7 4 C A A A
+  === END EMBEDDED FILE HEADER === #>
+```
+
+### How does it work ?
+
+#### Include a resource in a file
+
+The function ```ConvertTo-HeaderBlock``` takes a file path, then:
+
+1. will get all the characters with encoding 'UTF8', and convert them to a byte array
+2. compress the byte array using GZIP
+3. convert the data to Base 64 for text representation
+4. split the data in similar subgroups to have a pretty header block
+
+All you need to do afterwards is to include that block of text whereever you want in the file of your choice.
+
+***NOTE THAT WHEN YOU EXTRACT THE DATA FROM THE FILE IT WAS INCLUDED IN, THE LOCATION OF THE TEXT BLOCK IS NOT IMPORTANT, CAN BE AT THE BEGINNING, MID or END OF FILE...***
+
+#### Retrieve a resoure from a file
+
+
+The function ```ConvertFrom-HeaderBlock``` takes a file path, then:
+
+1. locate the text block that contains the resource in the file specified.
+2. convert Base64 to byte array
+3. decompress the bytes using GZIP
+4. convert back to text
--- scripts/Converter.ps1
+++ scripts/Converter.ps1
@@ -26,11 +26,154 @@
 # PATHS VARIABLES and DEFAULTS VALUES
 ################################################################################################
 
-$Global:StartTag = "=== BEGIN FILE VERSION HEADER ==="
-$Global:EndTag  = "=== END FILE VERSION HEADER ==="
-$Global:HeaderStart = "<# $Global:StartTag `n"
-$Global:HeaderEnd = "`n$Global:EndTag #>"
 
+
+$Global:StartTag = "=== BEGIN EMBEDDED FILE HEADER ==="
+$Global:EndTag  = "=== END EMBEDDED FILE HEADER ==="
+$Global:HeaderStart = "<# $Global:StartTag `n"
+$Global:HeaderEnd = "`n$Global:EndTag #>"   
+
+Function Convert-Base64ToBytes([string]$base64) {
+    [string]$padded = ''
+    if (($base64.Length % 4) -eq 0) {
+        $padded = $base64.Clone()
+    }else {
+        $padded = $base64 + "====".Substring($base64.Length % 4)
+    }
+     
+    $base64 = $padded.Replace("_", "/").Replace("-", "+")
+    return [System.Convert]::FromBase64String($base64)
+}
+
+Function Convert-Base64ToString([string]$base64) {
+    return [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($base64))
+}
+
+Function Convert-StringToBase64([string]$plain) {
+    return Convert-BytesToBase64([Text.Encoding]::UTF8.GetBytes($plain))
+}
+
+
+Function Convert-BytesToBase64([byte[]]$ByteArray) {
+    return [Convert]::ToBase64String($ByteArray)
+}
+
+
+Function Compress-ByteArray([byte[]]$ByteArray, [bool]$IsBinary) {
+    # Compress Byte array (gzip)Encoding 'UTF8'
+
+    [System.IO.MemoryStream]$CompressedMemoryStream = new-object System.IO.MemoryStream(,$ByteArray)
+    [System.IO.Compression.GzipStream]$GzipStream = [System.IO.Compression.GzipStream]::new($CompressedMemoryStream, ([System.IO.Compression.CompressionMode]::Compress))
+    #$GzipStream.Write($BinaryMemoryStream.GetBuffer(), 0, $BinaryMemoryStream.GetBuffer().Count)
+    $GzipStream.Write($ByteArray,0,$ByteArray.Count)
+    $GzipStream.Close()
+   
+    $ScriptBlockCompressed = $CompressedMemoryStream.ToArray()
+    return $ScriptBlockCompressed
+}
+
+Function Expand-CompressedBytes([byte[]]$CompressedBytes) {
+    $InputStream = New-Object System.IO.MemoryStream(, $CompressedBytes)
+    $MemoryStream = New-Object System.IO.MemoryStream
+    $GzipStream = New-Object System.IO.Compression.GzipStream $InputStream, ([System.IO.Compression.CompressionMode]::Decompress)
+    $GzipStream.CopyTo($MemoryStream)
+
+    [Byte[]]$DecompressedByteArray = $MemoryStream.ToArray()
+
+    $GzipStream.Close()
+    $MemoryStream.Close()
+    $InputStream.Close()
+    $DecompressedByteArray
+}
+
+
+
+function Get-ContentBytes{ 
+    [CmdletBinding(SupportsShouldProcess)]
+    Param (
+        [parameter(Mandatory=$true,Position=0)]
+        [string]$Path,
+        [parameter(Mandatory=$false,Position=1)]
+        [Int64]$Count=0
+    )
+    if($PSVersionTable.PSEdition -eq 'Core'){
+        if($Count -ne 0){
+            return ((Get-Content -Path "$Path" -AsByteStream -TotalCount $Count) -As [byte[]])
+        }else{
+            return ((Get-Content -Path "$Path" -AsByteStream) -As [byte[]])
+        }
+    }else{
+        if($Count -ne 0){
+            return ((Get-Content -Path "$Path" -Encoding Byte -TotalCount $Count) -As [byte[]])
+        }else{
+            return ((Get-Content -Path "$Path" -Encoding Byte) -As [byte[]])
+        }
+        
+    }
+
+}
+
+
+<#
+    .Synopsis
+        get the type of encoding for the target file. 
+    .Description
+        get the type of encoding for the target file. 
+    .Parameter Path
+        the target file
+    .Notes 
+        Returns one of those:
+        - binary : binary file
+        - ascii: Uses the encoding for the ASCII (7-bit) character set.
+        - bigendianunicode: Encodes in UTF-16 format using the big-endian byte order.
+        - bigendianutf32: Encodes in UTF-32 format using the big-endian byte order.
+        - unicode: Encodes in UTF-16 format using the little-endian byte order.
+        - utf8: Encodes in UTF-8 format.
+        - utf32: Encodes in UTF-32 format.
+#>
+function Get-FileEncoding{ 
+
+    [CmdletBinding(SupportsShouldProcess)]
+    Param (
+        [parameter(Mandatory=$true,Position=0)]
+        [string]$Path
+    )
+
+    # This should be ASCII encoded 
+    $encoding = "ASCII"
+
+    # Get the first 1024 bytes from the file
+    $byteArray = Get-ContentBytes -Path $Path -Count 1024
+
+    if( ("{0:X}{1:X}{2:X}" -f $byteArray) -eq "EFBBBF" ){
+        # Test for UTF-8 BOM
+        $encoding = "utf8"
+    }elseif( ("{0:X}{1:X}" -f $byteArray) -eq "FFFE" ){
+        # Test for the UTF-16
+        $encoding = "unicode"
+    }elseif( ("{0:X}{1:X}" -f $byteArray) -eq "FEFF" ){
+        # Test for the UTF-16 Big Endian
+        $encoding = "bigendianunicode"
+    }elseif( ("{0:X}{1:X}{2:X}{3:X}" -f $byteArray) -eq "FFFE0000" ){
+        # Test for the UTF-32
+        $encoding = "utf32"
+    }elseif( ("{0:X}{1:X}{2:X}{3:X}" -f $byteArray) -eq "0000FEFF" ){
+        # Test for the UTF-32 Big Endian
+        $encoding = "bigendianutf32"
+    }
+
+    # So now we're done with Text encodings that commonly have '0's
+    # in their byte steams.  ASCII may have the NUL or '0' code in
+    # their streams but that's rare apparently.
+
+    # Both GNU Grep and Diff use variations of this heuristic
+
+    if( $byteArray -contains 0 ){
+        $encoding = "binary"
+    }
+
+    return $encoding
+}
 
 
 function Convert-ToSmallerArray {
@@ -51,17 +194,19 @@ function Convert-ToSmallerArray {
             [Parameter(Mandatory=$true,Position=0)]
             [Array]$BigArray,
             [Parameter(Mandatory=$false)]
-            [int]$NumParts,
+            [int]$NumParts = 0,
             [Parameter(Mandatory=$false)]
-            [int]$RequestedSize
+            [int]$RequestedSize=0
         )
 
+    if ($NumParts -lt 1) { $NumParts = 1 }
+    if ($RequestedSize -lt 1) { $RequestedSize = 1 }
 
-    if ($NumParts) {
+    if ($NumParts -gt 1) {
         $PartSize = [Math]::Ceiling($BigArray.count / $NumParts)
         Write-Verbose "ToSmallerArray => NumParts is $NumParts"
     }
-    if ($RequestedSize) {
+    if ($RequestedSize -gt 1) {
         $PartSize = $RequestedSize
         $NumParts = [Math]::Ceiling($BigArray.count / $RequestedSize)
         Write-Verbose "ToSmallerArray => RequestedSize is $RequestedSize"
@@ -97,50 +242,56 @@ function ConvertFrom-HeaderBlock {
         [String]$Path
         
     )
-    $CompleteString = ''
-    $FileData = Get-Content -Path $Path -Raw -Encoding 'UTF8'
-    $index = $FileData.IndexOf($Global: )
-    $endindex = $FileData.IndexOf($Global:EndTag)
+
+    Write-Verbose "[ConvertFrom-HeaderBlock] `"$Path`" Type $Type"
+    [string]$FileStringData = Get-Content -Path "$Path" -Encoding utf8
+    $index = $FileStringData.IndexOf($Global:StartTag)
+    $endindex = $FileStringData.IndexOf($Global:EndTag)
     $index += 34
 
-    [char[]] $CArray = $FileData.ToCharArray() 
+    [char[]] $CArray = $FileStringData.ToCharArray() 
     $iMax = $CArray.Count
 
     Write-Verbose "index $index "
     Write-Verbose "endindex $endindex "
     Write-Verbose "iMax $iMax "
 
-
     For($i = $index ; $i -lt $endindex ; $i++){
-        #$CompleteString += $CArray[$i]
         if(($CArray[$i] -ne ' ') -and ($CArray[$i])){
             [char]$c = $CArray[$i]
             $CompleteString += $c
         }
-        #Write-Verbose "$CArray[$i]"
     }
 
- #   $CompleteString = [string]::join("",($CompleteString.Split("`n")))
+    $CompleteStringLen = $CompleteString.Length
+    Write-Verbose "[ConvertFrom-HeaderBlock]  CompleteString Length $CompleteStringLen"
+    [byte[]]$ScriptBlockCompressed = Convert-Base64ToBytes($CompleteString)
+    [Byte[]]$ScriptBlockEncoded = Expand-CompressedBytes -CompressedBytes $ScriptBlockCompressed
    
+    $HeaderSize = 1 + 8 + 8
 
-    $ScriptBlockCompressed = [System.Convert]::FromBase64String($CompleteString)
+    [System.IO.MemoryStream]$MemoryStream = [System.IO.MemoryStream]::new($ScriptBlockEncoded)   
+    [System.IO.BinaryReader]$BinaryReader = [System.IO.BinaryReader]::new($MemoryStream)
+    [bool]$IsBinary = $BinaryReader.ReadBoolean()
+    [UInt64]$DataBufferSize = $BinaryReader.ReadUInt64()
+    [Byte[]]$PlaceHolder = $BinaryReader.ReadBytes(8)
+    [Byte[]]$RawByteArray = $BinaryReader.ReadBytes($DataBufferSize)
 
-    # Decompress data
-    $InputStream = New-Object System.IO.MemoryStream(, $ScriptBlockCompressed)
-    $MemoryStream = New-Object System.IO.MemoryStream
-    $GzipStream = New-Object System.IO.Compression.GzipStream $InputStream, ([System.IO.Compression.CompressionMode]::Decompress)
-    $GzipStream.CopyTo($MemoryStream)
-    $GzipStream.Close()
-    $MemoryStream.Close()
-    $InputStream.Close()
-    [Byte[]] $ScriptBlockEncoded = $MemoryStream.ToArray()
+    Write-Verbose "[ConvertFrom-HeaderBlock]  ScriptBlock Compressed Size $($ScriptBlockCompressed.Count)"
+    Write-Verbose "[ConvertFrom-HeaderBlock]  ScriptBlock Expanded   Size $($ScriptBlockEncoded.Count)"
+    Write-Verbose "[ConvertFrom-HeaderBlock]  IsBinary               $IsBinary"
+    Write-Verbose "[ConvertFrom-HeaderBlock]  DataBufferSize         $DataBufferSize"
 
-    # Byte array to String
-    [System.Text.Encoding] $Encoding = [System.Text.Encoding]::UTF8
-    [string]$s = $Encoding.GetString($ScriptBlockEncoded)
+    if($IsBinary -eq $False){
+        # Byte array to String
+        [System.Text.Encoding] $Encoding = [System.Text.Encoding]::UTF8
+        [string]$s = $Encoding.GetString($RawByteArray)
+        return $s
+    }
 
-    return $s
+    return $RawByteArray 
 }
+
 
 
 function ConvertTo-HeaderBlock {
@@ -157,33 +308,105 @@ function ConvertTo-HeaderBlock {
             return $true 
         })]
         [Parameter(Mandatory=$true,Position=0)]
-        [String]$Path
+        [String]$Path,
+        [Parameter(Mandatory=$false)]
+        [uint32]$SizeSections = 65
         
     )
+    $HeaderSize = 1 + 8 + 8
+    [bool]$IsBinary = ((Get-FileEncoding -Path $Path ) -eq 'binary')
+    [Byte[]]$ScriptBlockEncoded = Get-ContentBytes -Path $Path
+    $ScriptBlockEncodedSize = $ScriptBlockEncoded.Count
+    $BufferSize = $ScriptBlockEncodedSize + $HeaderSize
+    [Byte[]]$Buffer = [Byte[]]::new($BufferSize)
 
-    $FileData = Get-Content -Path $Path -Raw -Encoding 'UTF8'
-    # Script block as String to Byte array
-    [System.Text.Encoding] $Encoding = [System.Text.Encoding]::UTF8
-    [Byte[]] $ScriptBlockEncoded = $Encoding.GetBytes($FileData)
+    Write-Verbose "[ConvertTo-HeaderBlock] `"$Path`" Binary $IsBinary"
 
-    # Compress Byte array (gzip)
-    [System.IO.MemoryStream] $MemoryStream = New-Object System.IO.MemoryStream
-    $GzipStream = New-Object System.IO.Compression.GzipStream $MemoryStream, ([System.IO.Compression.CompressionMode]::Compress)
-    $GzipStream.Write($ScriptBlockEncoded, 0, $ScriptBlockEncoded.Length)
-    $GzipStream.Close()
-    $MemoryStream.Close()
-    $ScriptBlockCompressed = $MemoryStream.ToArray()
+    [System.IO.MemoryStream]$MemoryStream = [System.IO.MemoryStream]::new($Buffer,$True)   
+    [System.IO.BinaryWriter]$BinaryWriter = [System.IO.BinaryWriter]::new($MemoryStream)
+ 
+   
+    $BinaryWriter.Write($IsBinary -as [bool]) # 1
+    $BinaryWriter.Write($ScriptBlockEncodedSize -as [UInt64]) # 8
+    [byte[]]$PlaceHolder = [byte[]]::new(8)
+    $BinaryWriter.Write($PlaceHolder)
+    $BinaryWriter.Write($ScriptBlockEncoded)
+    $BinaryWriter.Close()
+    
+    [Byte[]]$DataWithHeader = $MemoryStream.ToArray()
 
+    [Byte[]]$ScriptBlockCompressed = Compress-ByteArray -ByteArray $DataWithHeader -IsBinary $IsBinary
+    [string]$ScriptBase64 = Convert-BytesToBase64($ScriptBlockCompressed)
+
+    Write-Verbose "[ConvertTo-HeaderBlock]  ScriptBlock Compressed Size $($ScriptBlockCompressed.Count)"
+    Write-Verbose "[ConvertTo-HeaderBlock]  ScriptBlock Expanded   Size $($ScriptBlockEncoded.Count)"
+    Write-Verbose "[ConvertTo-HeaderBlock]  ScriptBlock Base 64    Size $($ScriptBase64.Length)"
+    
+    [char[]] $CArray = $ScriptBase64.ToCharArray() 
+    $Parts = Convert-ToSmallerArray -BigArray $CArray -RequestedSize $SizeSections
+    Write-Verbose "[ConvertTo-HeaderBlock]  Convert-ToSmallerArray RequestedSize $SizeSections"
     [string]$HeaderData =  $Global:HeaderStart
-    # Byte array to Base64
-    $B64String = [System.Convert]::ToBase64String($ScriptBlockCompressed)
-    [char[]] $CArray = $B64String.ToCharArray() 
-    $Parts = Convert-ToSmallerArray $CArray -NumParts 30
-    $Parts | % { $HeaderData += "$_" }
+    $Parts | % { $HeaderData += "$_`n" }
     $HeaderData += $Global:HeaderEnd
-
-    #Set-Content -Path "c:\Tmp\Header.txt" -Value $HeaderData
 
     return $HeaderData
 }
+
+
+
+function Split-HeaderBlockData {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory=$true,Position=0)]
+        [String]$Data,
+        [Parameter(Mandatory=$false)]
+        [uint32]$SizeSections = 80
+        
+    )
+    [String]$NewDataString = $Data.Clone()
+    $l = $Global:HeaderStart.Length + 1
+    $s = 0
+    $oldSubStr = $NewDataString.Substring($s, $l)
+    $newSubStr = $oldSubStr.Replace("`n","")
+    $NewDataString = $NewDataString.Replace($oldSubStr, $newSubStr)
+
+    $s = $NewDataString.Length - $Global:HeaderEnd.Length + 1
+    $l = $Global:EndTag.Length + 1
+    $oldSubStr = $NewDataString.Substring($s, $l)
+    $newSubStr = $oldSubStr.Replace("`n","")
+    $NewDataString = $NewDataString.Replace($oldSubStr, $newSubStr)
+    [string]$TmpTagStart = $Global:StartTag.Replace("`n","")
+    [string]$TmpTagEnd = $Global:EndTag.Replace("`n","")
+    [String]$NewDataString = $NewDataString.Replace("$TmpTagStart","$TmpTagStart`n`n").Replace("$TmpTagEnd","`n$TmpTagEnd")             
+
+
+    $Max = [math]::Max($($Global:StartTag.Length),$($Global:EndTag.Length))
+    $Max += 10
+    if($SizeSections -lt $Max){$SizeSections = $Max}
+
+    $s = $Global:HeaderStart.Length + 1
+    [string]$TmpTagStart = $Global:HeaderStart.Replace("`n","")
+    [string]$TmpTagEnd = $Global:HeaderEnd.Replace("`n","")
+    $l = $NewDataString.Length - ($Global:HeaderStart.Length + 1) - ($Global:HeaderEnd.Length)
+    $MiddleString = $NewDataString.Replace("`n","").Replace("$TmpTagStart","").Replace("$TmpTagEnd","")   
+    
+    $char_array = $MiddleString.ToCharArray()
+    $FinalValue = ''
+    $count = 0
+    ForEach($c in $char_array){
+        if($count -lt $SizeSections){
+            $count++
+            $FinalValue += $c
+        }else{
+            $count = 0
+            $FinalValue += "`n"
+            $FinalValue += $c
+        }
+    }
+
+    $FinalValue = "{0}`n{1}`n{2}" -f $Global:HeaderStart, $FinalValue, $Global:HeaderEnd
+    $FinalValue
+}
+
 
